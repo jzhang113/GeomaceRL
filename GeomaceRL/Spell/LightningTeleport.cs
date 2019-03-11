9aff6ada1713d477e13c5d5fb4bff10b2e9c5f99
Jiahua Zhang <jiahuaz@yahoo.com>
2019-03-11 01:34:35 -0500

    Teleporting on top of something kills it

--- GeomaceRL/Spell/LightningTeleport.cs
+++ GeomaceRL/Spell/LightningTeleport.cs
@@ -9,17 +9,26 @@ namespace GeomaceRL.Spell
         public string Name => "Teleport";
         public string Abbrev => "TP";
         public SpellCost Cost => new SpellCost(Element.Lightning, (Constants.TELEPORT_MIN, Constants.TELEPORT_COST));
-        public TargetZone Zone => new TargetZone(TargetShape.Range, Constants.TELEPORT_RANGE);
+        public TargetZone Zone => new TargetZone(TargetShape.Range, Constants.TELEPORT_RANGE, 0, false);
 
         public ICommand Evoke(Actor.Actor source, IEnumerable<Loc> targets, (int, int) used)
         {
             var target = targets.First();
             int accuracy = Constants.TELEPORT_COST - used.Item1;
-            var landing = Game.MapHandler.GetPointsInRadius(target, accuracy).Where(point =>
-                Game.MapHandler.Field[point].IsWalkable);
+            var landing = Game.MapHandler.GetPointsInRadius(target, accuracy);
 
             int index = Game.Rand.Next(landing.Count());
-            var square = landing.ElementAt(index);
+            Loc square = landing.ElementAt(index);
+
+            Game.MapHandler.GetActor(square).MatchSome(actor =>
+            {
+                Game.MessagePanel.AddMessage($"Player lands on {actor.Name}");
+                actor.TriggerDeath();
+                if (actor is Actor.Bomber)
+                {
+                    Game.Player.TakeDamage((actor.Element, Constants.GEN_ATTACK * 2), actor.Pos);
+                }
+            });
 
             Game.MapHandler.SetActorPosition(source, square);
             return new WaitCommand(source);
