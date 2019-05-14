a242ea3599a09d59621262e1225ab21f91a177df
Jiahua Zhang <jiahuaz@yahoo.com>
2019-05-14 17:01:01 -0500

    Randomize player spells

---
+++ GeomaceRL/Spell/Heal.cs
@@ -0,0 +1,19 @@
+﻿using GeomaceRL.Command;
+using System.Collections.Generic;
+
+namespace GeomaceRL.Spell
+{
+    internal class Heal : ISpell
+    {
+        public string Name => "Heal";
+        public string Abbrev => "H";
+        public SpellCost Cost => new SpellCost(Element.Water, 3, Element.Lightning, 3);
+        public TargetZone Zone => new TargetZone(TargetShape.Self);
+
+        public ICommand Evoke(Actor.Actor source, IEnumerable<Loc> targets, (int, int) used)
+        {
+            source.Health = source.MaxHealth;
+            return new WaitCommand(source);
+        }
+    }
+}
